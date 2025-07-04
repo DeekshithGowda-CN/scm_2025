50135793eff3a0a105259affd2b19d70cf0f767a
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-07-04 22:58:52 +0530

    I added a Code of addition of two numbers

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	float num1, num2;
+	printf("Enter first number")
+		scanf("%f",&num1);
+	printf("Enter sewcond number ");
+	        scanf("%f",&num2);
+
+	printf("Addition : %.2f\n",num1+num2);
