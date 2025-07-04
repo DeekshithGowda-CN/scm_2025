2c6df7e5947e0d67f993e5030c11ff130973ca9c
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-07-04 22:13:39 +0530

    I have added the code for addition of two numbers

---
+++ calculator.cpp
@@ -0,0 +1,10 @@
+#include <stdios.h>
+int main(){
+	float num1, num2;
+
+	printf("Enter first number : ")
+		scanf("%f",&num1);
+	printf("Enter second number : ")
+		scanf("%f",&num2);
+
+	printf("Additionn : %.2f\n",num1+num2);
